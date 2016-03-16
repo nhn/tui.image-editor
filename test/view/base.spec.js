de94c09cc44b6fcef801af2c08bb1c1ddb06d1e9
minkyu-yi <minkyu.yi@nhnent.com>
2016-03-16 17:10:52 +0900

    test: view interface

--- test/view/base.spec.js
+++ test/view/base.spec.js
@@ -18,35 +18,52 @@ describe('View interface', function() {
         }).toThrowError(msg);
 
         expect(function() {
+            instance.postCommand();
+        }).toThrowError();
+    });
+
+    describe('getName', function() {
+        it('should throw an error if nameless', function() {
+            var msg = consts.messages.NO_VIEW_NAME;
+
+            expect(function() {
+                instance.getName();
+            }).toThrowError(msg);
+        });
+
+        it('should return name if named', function() {
+            instance.name = 'test';
+            expect(instance.getName()).toEqual('test');
+        });
+    });
+
+    describe('getElement', function() {
+        it('should throw an error if does not have an element', function() {
+            var msg = consts.messages.NO_ELEMENT;
+
+            expect(function() {
+                instance.getElement();
+            }).toThrowError(msg);
+        });
+
+        it('should return element if has', function() {
+            var $element = $('<div />');
+
+            instance.$element = $element;
+            expect(instance.getElement()).toEqual($element);
+        });
+    });
+
+    describe('destroy', function() {
+        it('should remove element', function() {
+            var msg = consts.messages.NO_ELEMENT;
+
+            instance.$element = $('<div />');
             instance.destroy();
-        }).not.toThrowError();
-    });
 
-    it('should throw error if nameless', function() {
-        var msg = consts.messages.NO_VIEW_NAME;
-
-        expect(function() {
-            instance.getName();
-        }).toThrowError(msg);
-    });
-
-    it('should return name if named', function() {
-        instance.name = 'test';
-        expect(instance.getName()).toEqual('test');
-    });
-
-    it('should throw error if does not have an element', function() {
-        var msg = consts.messages.NO_ELEMENT;
-
-        expect(function() {
-            instance.getElement();
-        }).toThrowError(msg);
-    });
-
-    it('should return if has an element', function() {
-        var $element = $('<div />');
-
-        instance.$element = $element;
-        expect(instance.getElement()).toEqual($element);
+            expect(function() {
+                instance.getElement();
+            }).toThrowError(msg);
+        });
     });
 });
