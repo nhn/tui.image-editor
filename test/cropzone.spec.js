d91cc8a64b12b17be136a4ab63fef11f43b8ae34
minkyu-yi <minkyu.yi@nhnent.com>
2016-04-05 18:53:37 +0900

    tests: cropzone.spec.js

--- test/cropzone.spec.js
+++ test/cropzone.spec.js
@@ -75,4 +75,166 @@ describe('Cropzone', function() {
         cropzone.setHeight(1);
         expect(cropzone.isValid()).toBe(true);
     });
+
+    it('"_calcTopLeftScalingSizeFromPointer()"' +
+        ' should return scaling size(position + dimension)', function() {
+
+        var mousePointerX, mousePointerY,
+            expected, actual;
+
+        mousePointerX = 20;
+        mousePointerY = 30;
+        expected = {
+            left: 20,
+            top: 30,
+            width: 90,
+            height: 80
+        };
+        actual = cropzone._calcTopLeftScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+
+        mousePointerX = -10;
+        mousePointerY = 0;
+        expected = {
+            left: 0,
+            top: 0,
+            width: 110,
+            height: 110
+        };
+        actual = cropzone._calcTopLeftScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+
+        mousePointerX = 200;
+        mousePointerY = 300;
+        expected = {
+            left: 109,
+            top: 109,
+            width: 1,
+            height: 1
+        };
+        actual = cropzone._calcTopLeftScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+    });
+
+    it('"_calcBottomRightScalingSizeFromPointer()"' +
+        ' should return scaling size(dimension)', function() {
+
+        var mousePointerX, mousePointerY,
+            expected, actual;
+
+        // mocking canvas
+        cropzone.canvas = {
+            width: 400,
+            height: 400
+        };
+
+        mousePointerX = 20;
+        mousePointerY = 30;
+        expected = {
+            width: 10,
+            height: 20
+        };
+        actual = cropzone._calcBottomRightScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+
+        mousePointerX = -10;
+        mousePointerY = 0;
+        expected = {
+            width: 1,
+            height: 1
+        };
+        actual = cropzone._calcBottomRightScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+
+        mousePointerX = 200;
+        mousePointerY = 300;
+        expected = {
+            width: 190,
+            height: 290
+        };
+        actual = cropzone._calcBottomRightScalingSizeFromPointer(mousePointerX, mousePointerY);
+        expect(actual).toEqual(expected);
+    });
+
+    it('"_makeScalingSettings()" ' +
+        'should return suitable position&dimension values from corner', function() {
+
+        var mockTL = {
+                width: 1,
+                height: 2,
+                left: 3,
+                top: 4
+            },
+            mockBR = {
+                width: 5,
+                height: 6
+            },
+            expected, actual;
+
+        cropzone.__corner = 'tl';
+        expected = {
+            width: 1,
+            height: 2,
+            left: 3,
+            top: 4
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'tr';
+        expected = {
+            width: 5,
+            height: 2,
+            top: 4
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'bl';
+        expected = {
+            width: 1,
+            height: 6,
+            left: 3
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'br';
+        expected = {
+            width: 5,
+            height: 6
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'ml';
+        expected = {
+            width: 1,
+            left: 3
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'mt';
+        expected = {
+            height: 2,
+            top: 4
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'mr';
+        expected = {
+            width: 5
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+
+        cropzone.__corner = 'mb';
+        expected = {
+            height: 6
+        };
+        actual = cropzone._makeScalingSettings(mockTL, mockBR);
+        expect(expected).toEqual(actual);
+    });
 });
