7206e190da955a47d7dff2c31be7e2b473a8a737
seonim-ryu <seonim.ryu@nhnent.com>
2016-07-15 10:54:55 +0900

    feat : 1.1.0

--- 1.1.0/js/code-snippet.js
+++ 1.1.0/js/code-snippet.js
@@ -1,4 +1,4 @@
-/*!code-snippet v1.0.7 | NHN Entertainment*/
+/*!code-snippet v1.0.6 | NHN Entertainment*/
 /**********
  * array.js
  **********/
@@ -769,8 +769,7 @@
      */
     CustomEvents.prototype._eachCtxHandlerItemByContainId = function(ctxEventsItem, id, iteratee) {
         tui.util.forEachOwnProperties(ctxEventsItem, function(handlerItem, handlerItemId) {
-            var keyPair = handlerItemId.split('_');
-            if (keyPair[0] === id || keyPair[1] === id) {
+            if (handlerItemId.indexOf(id) > -1) {
                 iteratee(handlerItem, handlerItemId);
             }
         });
@@ -789,7 +788,7 @@
      * @private
      */
     CustomEvents.prototype._eachCtxEventByHandler = function(handler, iteratee) {
-        var handlerId = tui.util.stamp(handler) + '',
+        var handlerId = tui.util.stamp(handler),
             eachById = this._eachCtxHandlerItemByContainId;
 
         this._eachCtxEvents(function(ctxEventsItem, eventKey) {
@@ -812,7 +811,7 @@
      * @private
      */
     CustomEvents.prototype._eachCtxEventByContext = function(context, iteratee) {
-        var contextId = tui.util.stamp(context) + '',
+        var contextId = tui.util.stamp(context),
             eachById = this._eachCtxHandlerItemByContainId;
 
         this._eachCtxEvents(function(ctxEventsItem, eventKey) {
@@ -1042,13 +1041,13 @@
         var ctxEvents = this._ctxEvents,
             hasArgs = tui.util.isExisty(eventName),
             matchEventName,
-            matchHandler;
+            matchHandler,
+            lenKey;
 
         this._eachCtxEventByContext(context, function(handlerItem, hanId, ctxItems, eventKey) {
-            var lenKey = eventKey.replace('_idx', '_len'),
-                originEventName = eventKey.replace(/(_idx|_len)$/, ''); 
+            lenKey = eventKey.replace('_idx', '_len');
 
-            matchEventName = hasArgs && tui.util.isString(eventName) && originEventName === eventName;
+            matchEventName = hasArgs && tui.util.isString(eventName) && eventKey.indexOf(eventName) > -1;
             matchHandler = hasArgs && tui.util.isFunction(eventName) && handlerItem.fn === eventName;
 
             if (!hasArgs || (matchEventName || matchHandler)) {
@@ -1120,7 +1119,7 @@
             // {eventName: handler}
             context = handler;
             tui.util.forEachOwnProperties(eventName, function(handler, name) {
-                 this.on(name, handler, context);
+                this.on(name, handler, context);
             }, this);
             return;
         } else if (tui.util.isString(eventName) && eventName.indexOf(' ') > -1) {
@@ -1579,157 +1578,157 @@
 
 (function(tui) {
 
-'use strict';
+    'use strict';
 
-/* istanbul ignore if */
-if (!tui) {
-    tui = window.tui = {};
-}
-if (!tui.util) {
-    tui.util = window.tui.util = {};
-}
-
-/**
- * Check whether the defineProperty() method is supported.
- * @type {boolean}
- */
-var isSupportDefinedProperty = (function () {
-    try {
-        Object.defineProperty({}, 'x', {});
-        return true;
-    } catch (e) {
-        return false;
+    /* istanbul ignore if */
+    if (!tui) {
+        tui = window.tui = {};
     }
-}());
-
-/**
- * A unique value of a constant.
- * @type {number}
- */
-var enumValue = 0;
-
-/**
- * Make a constant-list that has unique values.<br>
- * In modern browsers (except IE8 and lower),<br>
- *  a value defined once can not be changed.
- *
- * @param {...string | string[]} itemList Constant-list (An array of string is available)
- * @exports Enum
- * @constructor
- * @class
- * @memberof tui.util
- * @examples
- *  //create
- *  var MYENUM = new Enum('TYPE1', 'TYPE2');
- *  var MYENUM2 = new Enum(['TYPE1', 'TYPE2']);
- *
- *  //usage
- *  if (value === MYENUM.TYPE1) {
- *       ....
- *  }
- *
- *  //add (If a duplicate name is inputted, will be disregarded.)
- *  MYENUM.set('TYPE3', 'TYPE4');
- *
- *  //get name of a constant by a value
- *  MYENUM.getName(MYENUM.TYPE1); // 'TYPE1'이 리턴된다.
- *
- *  // In modern browsers (except IE8 and lower), a value can not be changed in constants.
- *  var originalValue = MYENUM.TYPE1;
- *  MYENUM.TYPE1 = 1234; // maybe TypeError
- *  MYENUM.TYPE1 === originalValue; // true
- *
- **/
-function Enum(itemList) {
-    if (itemList) {
-        this.set.apply(this, arguments);
-    }
-}
-
-/**
- * Define a constants-list
- * @param {...string| string[]} itemList Constant-list (An array of string is available)
- */
-Enum.prototype.set = function(itemList) {
-    var self = this;
-
-    if (!tui.util.isArray(itemList)) {
-        itemList = tui.util.toArray(arguments);
+    if (!tui.util) {
+        tui.util = window.tui.util = {};
     }
 
-    tui.util.forEach(itemList, function itemListIteratee(item) {
-        self._addItem(item);
-    });
-};
-
-/**
- * Return a key of the constant.
- * @param {number} value A value of the constant.
- * @returns {string|undefined} Key of the constant.
- */
-Enum.prototype.getName = function(value) {
-    var foundedKey,
-        self = this;
-
-    tui.util.forEach(this, function(itemValue, key) {
-        if (self._isEnumItem(key) && value === itemValue) {
-            foundedKey = key;
+    /**
+     * Check whether the defineProperty() method is supported.
+     * @type {boolean}
+     */
+    var isSupportDefinedProperty = (function () {
+        try {
+            Object.defineProperty({}, 'x', {});
+            return true;
+        } catch (e) {
             return false;
         }
-    });
+    }());
 
-    return foundedKey;
-};
+    /**
+     * A unique value of a constant.
+     * @type {number}
+     */
+    var enumValue = 0;
 
-/**
- * Create a constant.
- * @private
- * @param {string} name Constant name. (It will be a key of a constant)
- */
-Enum.prototype._addItem = function(name) {
-    var value;
-
-    if (!this.hasOwnProperty(name)) {
-        value = this._makeEnumValue();
-
-        if (isSupportDefinedProperty) {
-            Object.defineProperty(this, name, {
-                enumerable: true,
-                configurable: false,
-                writable: false,
-                value: value
-            });
-        } else {
-            this[name] = value;
+    /**
+     * Make a constant-list that has unique values.<br>
+     * In modern browsers (except IE8 and lower),<br>
+     *  a value defined once can not be changed.
+     *
+     * @param {...string | string[]} itemList Constant-list (An array of string is available)
+     * @exports Enum
+     * @constructor
+     * @class
+     * @memberof tui.util
+     * @examples
+     *  //create
+     *  var MYENUM = new Enum('TYPE1', 'TYPE2');
+     *  var MYENUM2 = new Enum(['TYPE1', 'TYPE2']);
+     *
+     *  //usage
+     *  if (value === MYENUM.TYPE1) {
+ *       ....
+ *  }
+     *
+     *  //add (If a duplicate name is inputted, will be disregarded.)
+     *  MYENUM.set('TYPE3', 'TYPE4');
+     *
+     *  //get name of a constant by a value
+     *  MYENUM.getName(MYENUM.TYPE1); // 'TYPE1'이 리턴된다.
+     *
+     *  // In modern browsers (except IE8 and lower), a value can not be changed in constants.
+     *  var originalValue = MYENUM.TYPE1;
+     *  MYENUM.TYPE1 = 1234; // maybe TypeError
+     *  MYENUM.TYPE1 === originalValue; // true
+     *
+     **/
+    function Enum(itemList) {
+        if (itemList) {
+            this.set.apply(this, arguments);
         }
     }
-};
 
-/**
- * Return a unique value for assigning to a constant.
- * @private
- * @returns {number} A unique value
- */
-Enum.prototype._makeEnumValue = function() {
-    var value;
+    /**
+     * Define a constants-list
+     * @param {...string| string[]} itemList Constant-list (An array of string is available)
+     */
+    Enum.prototype.set = function(itemList) {
+        var self = this;
 
-    value = enumValue;
-    enumValue += 1;
+        if (!tui.util.isArray(itemList)) {
+            itemList = tui.util.toArray(arguments);
+        }
 
-    return value;
-};
+        tui.util.forEach(itemList, function itemListIteratee(item) {
+            self._addItem(item);
+        });
+    };
 
-/**
- * Return whether a constant from the given key is in instance or not.
- * @param {string} key - A constant key
- * @returns {boolean} Result
- * @private
- */
-Enum.prototype._isEnumItem = function(key) {
-    return tui.util.isNumber(this[key]);
-};
+    /**
+     * Return a key of the constant.
+     * @param {number} value A value of the constant.
+     * @returns {string|undefined} Key of the constant.
+     */
+    Enum.prototype.getName = function(value) {
+        var foundedKey,
+            self = this;
 
-tui.util.Enum = Enum;
+        tui.util.forEach(this, function(itemValue, key) {
+            if (self._isEnumItem(key) && value === itemValue) {
+                foundedKey = key;
+                return false;
+            }
+        });
+
+        return foundedKey;
+    };
+
+    /**
+     * Create a constant.
+     * @private
+     * @param {string} name Constant name. (It will be a key of a constant)
+     */
+    Enum.prototype._addItem = function(name) {
+        var value;
+
+        if (!this.hasOwnProperty(name)) {
+            value = this._makeEnumValue();
+
+            if (isSupportDefinedProperty) {
+                Object.defineProperty(this, name, {
+                    enumerable: true,
+                    configurable: false,
+                    writable: false,
+                    value: value
+                });
+            } else {
+                this[name] = value;
+            }
+        }
+    };
+
+    /**
+     * Return a unique value for assigning to a constant.
+     * @private
+     * @returns {number} A unique value
+     */
+    Enum.prototype._makeEnumValue = function() {
+        var value;
+
+        value = enumValue;
+        enumValue += 1;
+
+        return value;
+    };
+
+    /**
+     * Return whether a constant from the given key is in instance or not.
+     * @param {string} key - A constant key
+     * @returns {boolean} Result
+     * @private
+     */
+    Enum.prototype._isEnumItem = function(key) {
+        return tui.util.isNumber(this[key]);
+    };
+
+    tui.util.Enum = Enum;
 
 })(window.tui);
 
@@ -2582,21 +2581,21 @@ tui.util.Enum = Enum;
     // Caching tui.util for performance enhancing
     var util = tui.util,
 
-    /**
-     * Using undefined for a key can be ambiguous if there's deleted item in the array,<br>
-     * which is also undefined when accessed by index.<br>
-     * So use this unique object as an undefined key to distinguish it from deleted keys.
-     * @private
-     * @constant
-     */
-    _KEY_FOR_UNDEFINED = {},
+        /**
+         * Using undefined for a key can be ambiguous if there's deleted item in the array,<br>
+         * which is also undefined when accessed by index.<br>
+         * So use this unique object as an undefined key to distinguish it from deleted keys.
+         * @private
+         * @constant
+         */
+        _KEY_FOR_UNDEFINED = {},
 
-    /**
-     * For using NaN as a key, use this unique object as a NaN key.<br>
-     * This makes it easier and faster to compare an object with each keys in the array<br>
-     * through no exceptional comapring for NaN.
-     */
-    _KEY_FOR_NAN = {};
+        /**
+         * For using NaN as a key, use this unique object as a NaN key.<br>
+         * This makes it easier and faster to compare an object with each keys in the array<br>
+         * through no exceptional comapring for NaN.
+         */
+        _KEY_FOR_NAN = {};
 
     /**
      * Constructor of MapIterator<br>
@@ -2621,8 +2620,8 @@ tui.util.Enum = Enum;
     MapIterator.prototype.next = function() {
         var data = {};
         do {
-           this._index += 1;
-       } while (util.isUndefined(this._keys[this._index]) && this._index < this._length);
+            this._index += 1;
+        } while (util.isUndefined(this._keys[this._index]) && this._index < this._length);
 
         if (this._index >= this._length) {
             data.done = true;
@@ -2877,7 +2876,7 @@ tui.util.Enum = Enum;
      * Removes the specified element from a Map object.
      * @param {*} key - The key of the element to remove
      */
-     // cannot use reserved keyword as a property name in IE8 and under.
+        // cannot use reserved keyword as a property name in IE8 and under.
     Map.prototype['delete'] = function(key) {
         var keyIndex;
 
@@ -3515,7 +3514,7 @@ tui.util.Enum = Enum;
      *  tui.util.isExisty({}); //true
      *  tui.util.isExisty(null); //false
      *  tui.util.isExisty(undefined); //false
-    */
+     */
     function isExisty(param) {
         return param != null;
     }
