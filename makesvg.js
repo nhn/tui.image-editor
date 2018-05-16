db4b865ad0a8052febfc04e3654c61ef34567d69
김진우 <jw.kim@nhnent.com>
2018-05-17 01:19:15 +0900

    fixed makesvg

--- makesvg.js
+++ makesvg.js
@@ -8,6 +8,7 @@ function getFileList(dir) {
     fs.readdir(targetDir, (err, files) => {
         if (!files) return;
         files.forEach(file => {
+            if (file.match(/^\./)) return;
             const id = `${dir}-${file.replace(/\.svg$/, '')}`;
             const svg = fs.readFileSync(targetDir + '/' + file);
             sprites.add(id, svg);
