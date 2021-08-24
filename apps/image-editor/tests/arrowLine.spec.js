fe92253b75ecf8b9e5212aefe00c9a1f8377dfa6
lja1018 <jaeeon.lim@nhn.com>
2021-08-24 16:47:33 +0900

    refactor: apply jest to `arrowLine.spec.js`

--- apps/image-editor/tests/arrowLine.spec.js
+++ apps/image-editor/tests/arrowLine.spec.js
@@ -1,19 +1,16 @@
-/**
- * @author NHN. FE Development Team <dl_javascript@nhn.com>
- * @fileoverview Test cases of "src/js/extension/allowLine.js"
- */
 import ArrowLine from '@/extension/arrowLine';
 
-describe('AllowLine', () => {
+describe('ArrowLine', () => {
+  const transformFn = (value) => Math.round(value);
   let ctx, arrowLine, linePath;
 
   beforeEach(() => {
     ctx = {
       lineWidth: 1,
-      beginPath: jasmine.createSpy('beginPath'),
-      moveTo: jasmine.createSpy('moveTo'),
-      lineTo: jasmine.createSpy('lineTo'),
-      closePath: jasmine.createSpy('closePath'),
+      beginPath: jest.fn(),
+      moveTo: jest.fn(),
+      lineTo: jest.fn(),
+      closePath: jest.fn(),
     };
     arrowLine = new ArrowLine();
     arrowLine.ctx = ctx;
@@ -25,49 +22,43 @@ describe('AllowLine', () => {
     };
   });
 
-  it('When attaching the "chevron" type to the endpoint, you need to draw the "v" calculated according to the angle around the "tail" of the line.', () => {
-    arrowLine.arrowType = {
-      tail: 'chevron',
-    };
+  it('should draw the "v" calculated according to the angle around the "tail" of the line when attaching the "chevron" type to the end point', () => {
+    arrowLine.arrowType = { tail: 'chevron' };
     arrowLine._drawDecoratorPath(linePath);
 
-    const firstPoint = ctx.moveTo.calls.argsFor(0).map((value) => Math.round(value));
-    const secondPoint = ctx.lineTo.calls.argsFor(0).map((value) => Math.round(value));
-    const lastPoint = ctx.lineTo.calls.argsFor(1).map((value) => Math.round(value));
+    const firstPoint = ctx.moveTo.mock.calls[0].map(transformFn);
+    const secondPoint = ctx.lineTo.mock.calls[0].map(transformFn);
+    const lastPoint = ctx.lineTo.mock.calls[1].map(transformFn);
 
     expect(firstPoint).toEqual([9, 7]);
     expect(secondPoint).toEqual([10, 10]);
     expect(lastPoint).toEqual([7, 9]);
   });
 
-  it('When attaching the "chevron" type to the startpoint, you need to draw the "v" calculated according to the angle around the "head" of the line.', () => {
-    arrowLine.arrowType = {
-      head: 'chevron',
-    };
+  it('should draw the "v" calculated according to the angle around the "head" of the line when attaching the "chevron" type to the start point', () => {
+    arrowLine.arrowType = { head: 'chevron' };
     arrowLine._drawDecoratorPath(linePath);
 
-    const firstPoint = ctx.moveTo.calls.argsFor(0).map((value) => Math.round(value));
-    const secondPoint = ctx.lineTo.calls.argsFor(0).map((value) => Math.round(value));
-    const lastPoint = ctx.lineTo.calls.argsFor(1).map((value) => Math.round(value));
+    const firstPoint = ctx.moveTo.mock.calls[0].map(transformFn);
+    const secondPoint = ctx.lineTo.mock.calls[0].map(transformFn);
+    const lastPoint = ctx.lineTo.mock.calls[1].map(transformFn);
 
     expect(firstPoint).toEqual([2, 4]);
     expect(secondPoint).toEqual([1, 1]);
     expect(lastPoint).toEqual([4, 2]);
   });
 
-  it('"triangle" should be a triangular shape that closes the path with closePath after drawing.', () => {
-    arrowLine.arrowType = {
-      head: 'triangle',
-    };
+  it('should be a triangular shape that closes the path with closePath after drawing', () => {
+    arrowLine.arrowType = { head: 'triangle' };
     arrowLine._drawDecoratorPath(linePath);
 
-    const firstPoint = ctx.moveTo.calls.argsFor(0).map((value) => Math.round(value));
-    const secondPoint = ctx.lineTo.calls.argsFor(0).map((value) => Math.round(value));
-    const thirdPoint = ctx.lineTo.calls.argsFor(1).map((value) => Math.round(value));
+    const firstPoint = ctx.moveTo.mock.calls[0].map(transformFn);
+    const secondPoint = ctx.lineTo.mock.calls[0].map(transformFn);
+    const lastPoint = ctx.lineTo.mock.calls[1].map(transformFn);
 
     expect(firstPoint).toEqual([1, 3]);
     expect(secondPoint).toEqual([1, 1]);
-    expect(thirdPoint).toEqual([3, 1]);
-    expect(ctx.closePath.calls.count()).toBe(1);
+    expect(lastPoint).toEqual([3, 1]);
+    expect(ctx.closePath).toBeCalledTimes(1);
   });
 });
