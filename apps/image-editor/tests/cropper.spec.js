026eb976c90feb2a6de840cce3fa0a65624a8f25
lja1018 <jaeeon.lim@nhn.com>
2021-08-24 11:51:54 +0900

    refactor: apply jest to `cropper.spec.js`

--- apps/image-editor/tests/cropper.spec.js
+++ apps/image-editor/tests/cropper.spec.js
@@ -1,7 +1,3 @@
-/**
- * @author NHN. FE Development Team <dl_javascript@nhn.com>
- * @fileoverview Test cases of "src/js/component/cropper.js"
- */
 import snippet from 'tui-code-snippet';
 import { fabric } from 'fabric';
 import Graphics from '@/graphics';
@@ -12,7 +8,7 @@ describe('Cropper', () => {
   let cropper, graphics, canvas;
 
   beforeEach(() => {
-    graphics = new Graphics($('<canvas>')[0]);
+    graphics = new Graphics(document.createElement('canvas'));
     canvas = graphics.getCanvas();
     cropper = new Cropper(graphics);
   });
@@ -34,102 +30,94 @@ describe('Cropper', () => {
     });
 
     it('should add a cropzone to canvas', () => {
-      spyOn(canvas, 'add');
+      const addSpy = jest.spyOn(canvas, 'add');
       cropper.start();
 
-      expect(canvas.add).toHaveBeenCalledWith(cropper._cropzone);
+      expect(addSpy).toHaveBeenCalledWith(cropper._cropzone);
     });
 
     it('should no action if a croppzone has been defined', () => {
       cropper._cropzone = {};
-      spyOn(canvas, 'add');
+      const addSpy = jest.spyOn(canvas, 'add');
       cropper.start();
 
-      expect(canvas.add).not.toHaveBeenCalled();
+      expect(addSpy).not.toHaveBeenCalled();
     });
 
     it('should set "evented" of all objects to false', () => {
+      const eventedOptions = { evented: true };
       const objects = [
-        new fabric.Rect({ evented: true }),
-        new fabric.Rect({ evented: true }),
-        new fabric.Rect({ evented: true }),
+        new fabric.Rect(eventedOptions),
+        new fabric.Rect(eventedOptions),
+        new fabric.Rect(eventedOptions),
       ];
-      canvas.add(objects[0], objects[1], objects[2]);
+      canvas.add(...objects);
 
       cropper.start();
+
       expect(objects[0].evented).toBe(false);
       expect(objects[1].evented).toBe(false);
       expect(objects[2].evented).toBe(false);
     });
   });
 
-  describe('"onFabricMouseDown()"', () => {
+  describe('onFabricMouseDown()', () => {
     let fEvent;
+
     beforeEach(() => {
-      fEvent = {
-        e: {},
-      };
-      spyOn(canvas, 'getPointer').and.returnValue({
-        x: 10,
-        y: 20,
-      });
+      fEvent = { e: {} };
+      jest.spyOn(canvas, 'getPointer').mockReturnValue({ x: 10, y: 20 });
     });
 
     it('should set "selection" to false', () => {
       cropper._onFabricMouseDown(fEvent);
+
       expect(canvas.selection).toBe(false);
     });
 
     it('should set "startX, startY"', () => {
-      // canvas.getPointer will return object{x: 10, y: 20}
       cropper._onFabricMouseDown(fEvent);
+
       expect(cropper._startX).toEqual(10);
       expect(cropper._startY).toEqual(20);
     });
   });
 
-  describe('"onFabricMouseMove()', () => {
+  describe('onFabricMouseMove()', () => {
     beforeEach(() => {
-      spyOn(canvas, 'getPointer').and.returnValue({
-        x: 10,
-        y: 20,
-      });
-      spyOn(canvas, 'getWidth').and.returnValue(100);
-      spyOn(canvas, 'getHeight').and.returnValue(200);
+      jest.spyOn(canvas, 'getPointer').mockReturnValue({ x: 10, y: 20 });
+      jest.spyOn(canvas, 'getWidth').mockReturnValue(100);
+      jest.spyOn(canvas, 'getHeight').mockReturnValue(200);
     });
 
-    it(
-      'should re-render(remove->set->add) cropzone ' +
-        'if the mouse moving is over the threshold(=10)',
-      () => {
-        cropper._startX = 0;
-        cropper._startY = 0;
+    it('should re-render(remove->set->add) cropzone if the mouse moving is over the threshold(=10)', () => {
+      cropper._startX = 0;
+      cropper._startY = 0;
 
-        cropper.start();
-        spyOn(canvas, 'remove');
-        spyOn(cropper._cropzone, 'set');
-        spyOn(canvas, 'add');
-        cropper._onFabricMouseMove({ e: {} });
+      cropper.start();
+      const removeSpy = jest.spyOn(canvas, 'remove');
+      const setSpy = jest.spyOn(cropper._cropzone, 'set');
+      const addSpy = jest.spyOn(canvas, 'add');
+      cropper._onFabricMouseMove({ e: {} });
 
-        expect(canvas.remove).toHaveBeenCalled();
-        expect(cropper._cropzone.set).toHaveBeenCalled();
-        expect(canvas.add).toHaveBeenCalled();
-      }
-    );
+      expect(removeSpy).toHaveBeenCalled();
+      expect(setSpy).toHaveBeenCalled();
+      expect(addSpy).toHaveBeenCalled();
+    });
 
-    it('should not re-render cropzone ' + 'if the mouse moving is under the threshold', () => {
+    it('should not re-render cropzone if the mouse moving is under the threshold', () => {
       cropper._startX = 14;
       cropper._startY = 18;
 
       cropper.start();
-      spyOn(canvas, 'remove');
-      spyOn(cropper._cropzone, 'set');
-      spyOn(canvas, 'add');
+      const removeSpy = jest.spyOn(canvas, 'remove');
+      const setSpy = jest.spyOn(cropper._cropzone, 'set');
+      const addSpy = jest.spyOn(canvas, 'add');
       cropper._onFabricMouseMove({ e: {} });
 
-      expect(canvas.remove).not.toHaveBeenCalled();
-      expect(cropper._cropzone.set).not.toHaveBeenCalled();
-      expect(canvas.add).not.toHaveBeenCalled();
+      expect(removeSpy).not.toHaveBeenCalled();
+      expect(setSpy).not.toHaveBeenCalled();
+      expect(addSpy).not.toHaveBeenCalled();
     });
   });
 
@@ -137,121 +125,105 @@ describe('Cropper', () => {
     beforeEach(() => {
       cropper._startX = 10;
       cropper._startY = 20;
-      snippet.extend(canvas, {
-        getWidth() {
-          return 100;
-        },
-        getHeight() {
-          return 200;
-        },
-      });
+      jest.spyOn(canvas, 'getWidth').mockReturnValue(100);
+      jest.spyOn(canvas, 'getHeight').mockReturnValue(200);
     });
 
     it('should return cropzone-left&top (min: 0, max: startX,Y)', () => {
-      const x = 20,
-        y = -1,
-        expected = {
-          left: 10,
-          top: 0,
-          width: jasmine.any(Number),
-          height: jasmine.any(Number),
-        },
-        actual = cropper._calcRectDimensionFromPoint(x, y);
+      const dimension = cropper._calcRectDimensionFromPoint(20, -1);
 
-      expect(actual).toEqual(expected);
+      expect(dimension).toEqual({
+        left: 10,
+        top: 0,
+        width: expect.any(Number),
+        height: expect.any(Number),
+      });
     });
 
     it('should calculate and return cropzone-width&height', () => {
-      let x, y, expected, actual;
+      let dimension;
 
-      x = 30;
-      y = 40;
-      expected = {
+      dimension = cropper._calcRectDimensionFromPoint(30, 40);
+      expect(dimension).toEqual({
         left: 10,
         top: 20,
         width: 20,
         height: 20,
-      };
-      actual = cropper._calcRectDimensionFromPoint(x, y);
-      expect(actual).toEqual(expected);
+      });
 
-      x = 300;
-      y = 400;
-      expected = {
+      dimension = cropper._calcRectDimensionFromPoint(300, 400);
+      expect(dimension).toEqual({
         left: 10,
         top: 20,
         width: 90,
         height: 180,
-      };
-      actual = cropper._calcRectDimensionFromPoint(x, y);
-      expect(actual).toEqual(expected);
+      });
     });
 
     it('should create cropzone that has fixed ratio during shift key is pressed.', () => {
-      const x = 100;
-      const y = 200;
-      const expected = {
+      cropper._withShiftKey = true;
+
+      const dimension = cropper._calcRectDimensionFromPoint(100, 200);
+
+      expect(dimension).toEqual({
         left: 10,
         top: 20,
         width: 180,
         height: 180,
-      };
-
-      cropper._withShiftKey = true;
-
-      const actual = cropper._calcRectDimensionFromPoint(x, y);
-
-      expect(actual).toEqual(expected);
+      });
     });
 
     it('should create cropzone that inverted current mouse position during shift key is pressed.', () => {
-      const x = -10;
-      const y = -20;
-      const expected = {
+      cropper._withShiftKey = true;
+
+      const dimension = cropper._calcRectDimensionFromPoint(-10, -20);
+
+      expect(dimension).toEqual({
         left: -10,
         top: 0,
         width: 20,
         height: 20,
-      };
-
-      cropper._withShiftKey = true;
-
-      const actual = cropper._calcRectDimensionFromPoint(x, y);
-
-      expect(actual).toEqual(expected);
+      });
     });
   });
 
-  it('"onFabricMouseUp()" should activate cropzone', () => {
-    canvas.setActiveObject = jasmine.createSpy();
+  it('should activate cropzone', () => {
+    canvas.setActiveObject = jest.fn();
     cropper.start();
     cropper._onFabricMouseUp();
 
     expect(canvas.setActiveObject).toHaveBeenCalledWith(cropper._cropzone);
   });
 
-  describe('"crop()"', () => {
-    it('should return cropzone rect', () => {
+  describe('crop()', () => {
+    beforeEach(() => {
       cropper.start();
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+    });
 
-      expect(cropper.getCropzoneRect()).toBeTruthy();
+    afterEach(() => {
       cropper.end();
+    });
+
+    it('should return cropzone rect', () => {
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
+      const cropzoneRect = cropper.getCropzoneRect();
+
+      expect(cropzoneRect).not.toBeNull();
     });
 
     it('should return cropzone data if the cropzone is valid', () => {
-      cropper.start();
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
+      const cropzoneRect = cropper.getCropzoneRect();
+      const croppedImageData = cropper.getCroppedImageData(cropzoneRect);
 
-      expect(cropper.getCroppedImageData(cropper.getCropzoneRect())).toEqual({
-        imageName: jasmine.any(String),
-        url: jasmine.any(String),
+      expect(croppedImageData).toEqual({
+        imageName: expect.any(String),
+        url: expect.any(String),
       });
-      cropper.end();
     });
   });
 
-  describe('"presets - setCropzoneRect()"', () => {
+  describe('presets - setCropzoneRect()', () => {
     beforeEach(() => {
       cropper.start();
     });
@@ -261,94 +233,91 @@ describe('Cropper', () => {
     });
 
     it('should return cropzone rect as a square', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
-      cropper.setCropzoneRect(1 / 1);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
+      cropper.setCropzoneRect(1);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect(cropper.getCropzoneRect().width).toBe(cropper.getCropzoneRect().height);
+      expect(width).toBe(height);
     });
 
     it('should return cropzone rect as a 3:2 aspect box', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
       cropper.setCropzoneRect(3 / 2);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect((cropper.getCropzoneRect().width / cropper.getCropzoneRect().height).toFixed(1)).toBe(
-        (3 / 2).toFixed(1)
-      );
+      expect((width / height).toFixed(1)).toBe((3 / 2).toFixed(1));
     });
 
     it('should return cropzone rect as a 4:3 aspect box', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
       cropper.setCropzoneRect(4 / 3);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect((cropper.getCropzoneRect().width / cropper.getCropzoneRect().height).toFixed(1)).toBe(
-        (4 / 3).toFixed(1)
-      );
+      expect((width / height).toFixed(1)).toBe((4 / 3).toFixed(1));
     });
 
     it('should return cropzone rect as a 5:4 aspect box', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
       cropper.setCropzoneRect(5 / 4);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect((cropper.getCropzoneRect().width / cropper.getCropzoneRect().height).toFixed(1)).toBe(
-        (5 / 4).toFixed(1)
-      );
+      expect((width / height).toFixed(1)).toBe((5 / 4).toFixed(1));
     });
 
     it('should return cropzone rect as a 7:5 aspect box', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
       cropper.setCropzoneRect(7 / 5);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect((cropper.getCropzoneRect().width / cropper.getCropzoneRect().height).toFixed(1)).toBe(
-        (7 / 5).toFixed(1)
-      );
+      expect((width / height).toFixed(1)).toBe((7 / 5).toFixed(1));
     });
 
     it('should return cropzone rect as a 16:9 aspect box', () => {
-      spyOn(cropper._cropzone, 'isValid').and.returnValue(true);
+      jest.spyOn(cropper._cropzone, 'isValid').mockReturnValue(true);
       cropper.setCropzoneRect(16 / 9);
+      const { width, height } = cropper.getCropzoneRect();
 
-      expect((cropper.getCropzoneRect().width / cropper.getCropzoneRect().height).toFixed(1)).toBe(
-        (16 / 9).toFixed(1)
-      );
+      expect((width / height).toFixed(1)).toBe((16 / 9).toFixed(1));
     });
 
     it('Even in situations with floating point problems, should calculate the exact width you expect.', () => {
-      spyOn(canvas, 'getWidth').and.returnValue(408);
-      spyOn(canvas, 'getHeight').and.returnValue(312);
-      spyOn(cropper._cropzone, 'set').and.callThrough();
+      jest.spyOn(canvas, 'getWidth').mockReturnValue(408);
+      jest.spyOn(canvas, 'getHeight').mockReturnValue(312);
+      const setSpy = jest.spyOn(cropper._cropzone, 'set');
 
       cropper.setCropzoneRect(16 / 9);
 
-      expect(cropper._cropzone.set.calls.first().args[0].width).toBe(408);
+      expect(setSpy).toHaveBeenCalledWith(expect.objectContaining({ width: 408 }));
     });
 
     it('should remove cropzone of cropper when falsy is passed', () => {
       cropper.setCropzoneRect();
-      expect(cropper.getCropzoneRect()).toBeFalsy();
+      expect(cropper.getCropzoneRect()).toBeNull();
 
       cropper.setCropzoneRect(0);
-      expect(cropper.getCropzoneRect()).toBeFalsy();
+      expect(cropper.getCropzoneRect()).toBeNull();
 
       cropper.setCropzoneRect(null);
-      expect(cropper.getCropzoneRect()).toBeFalsy();
+      expect(cropper.getCropzoneRect()).toBeNull();
     });
   });
 
-  describe('"end()"', () => {
+  describe('end()', () => {
     it('should set cropzone of cropper to null', () => {
       cropper.start();
       cropper.end();
 
-      expect(cropper._cropzone).toBe(null);
+      expect(cropper._cropzone).toBeNull();
     });
 
-    it('should set "evented" of all obejcts to true', () => {
+    it('should set "evented" of all objects to true', () => {
+      const eventedOptions = { evented: false };
       const objects = [
-        new fabric.Rect({ evented: false }),
-        new fabric.Rect({ evented: false }),
-        new fabric.Rect({ evented: false }),
+        new fabric.Rect(eventedOptions),
+        new fabric.Rect(eventedOptions),
+        new fabric.Rect(eventedOptions),
       ];
-      canvas.add(objects[0], objects[1], objects[2]);
+      canvas.add(...objects);
 
       cropper.start();
       cropper.end();
@@ -357,22 +326,18 @@ describe('Cropper', () => {
       expect(objects[2].evented).toBe(true);
     });
   });
+
   describe('canvas event delegator', () => {
     it('The event of an object with an eventDelegator must fire the graphics.fire registered with the trigger.', () => {
       cropper.start();
-      spyOn(graphics, 'fire');
-      const events = eventNames;
-      const fEvent = {
-        target: cropper._cropzone,
-      };
-
+      const fireSpy = jest.spyOn(graphics, 'fire');
       const cropzone = cropper._cropzone;
 
-      canvas.fire('object:scaling', fEvent);
+      canvas.fire('object:scaling', { target: cropper._cropzone });
 
-      expect(graphics.fire.calls.count()).toBe(0);
-      cropzone.canvasEventTrigger[events.OBJECT_SCALED](cropzone);
-      expect(graphics.fire.calls.count()).toBe(1);
+      expect(fireSpy).not.toHaveBeenCalled();
+      cropzone.canvasEventTrigger[eventNames.OBJECT_SCALED](cropzone);
+      expect(fireSpy).toHaveBeenCalled();
     });
   });
 });
