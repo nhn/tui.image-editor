4f38fa4eb640cc43624d3ac6c462613440d8d660
lja1018 <jaeeon.lim@nhn.com>
2021-08-27 14:51:10 +0900

    refactor: remove unused env

--- apps/image-editor/tests/index.js
+++ apps/image-editor/tests/index.js
@@ -1,11 +1,3 @@
-/**
- * @author NHN. FE Development Lab <dl_javascript@nhn.com>
- * @fileoverview Test env
- */
-import '../src';
 import { fabric } from 'fabric';
 
 fabric.Object.prototype.objectCaching = false;
-
-const testsContext = require.context('.', true, /spec\.js$/);
-testsContext.keys().forEach(testsContext);
