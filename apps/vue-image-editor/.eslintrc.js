fae721eeee39493c77d6dd7321f636f44febfdd7
lja1018 <jaeeon.lim@nhn.com>
2021-09-03 12:13:53 +0900

    env: extract dependencies related `eslint` to root in `apps/vue-image-editor`
    * add `eslint-plugin-vue`
    * add `vue-eslint-parser`
    * add `vue`
    * remove `@vue/cli-service-global`
    
    # Conflicts:
    #	package-lock.json

--- apps/vue-image-editor/.eslintrc.js
+++ apps/vue-image-editor/.eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  extends: ['tui/es6', 'plugin:vue/base', 'plugin:prettier/recommended'],
+  extends: ['tui/es6', 'plugin:vue/recommended', 'plugin:prettier/recommended'],
+  plugins: ['vue', 'prettier'],
+  ignorePatterns: ['node_modules/*', 'dist'],
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
     ecmaVersion: 7,
     sourceType: 'module',
   },
-  plugins: ['vue', 'prettier'],
 };
