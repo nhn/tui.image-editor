2af4f057769f6ef3705643610c10609be2b9b68f
EdricLi <EdricLi@XA-DD3-EDRICLI>
2022-02-21 11:35:42 +0800

    The language environment is not case-sensitive when translated.

--- apps/image-editor/src/js/ui/locale/locale.js
+++ apps/image-editor/src/js/ui/locale/locale.js
@@ -3,7 +3,15 @@
  */
 class Locale {
   constructor(locale) {
-    this._locale = locale;
+    const newLocale = {};
+
+    for (const key in locale) {
+      if (Object.prototype.hasOwnProperty.call(locale, key)) {
+        newLocale[key.toUpperCase()] = locale[key];
+      }
+    }
+
+    this._locale = newLocale;
   }
 
   /**
@@ -12,7 +20,7 @@ class Locale {
    * @returns {string}
    */
   localize(message) {
-    return this._locale[message] || message;
+    return this._locale[message.toString().toUpperCase()] || message;
   }
 }
 
