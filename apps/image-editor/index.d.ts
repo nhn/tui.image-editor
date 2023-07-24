a5c2a317cadf74179e940ee990b3cc11872e06c8
Amirreza Zarkesh <56929398+ARZarkesh@users.noreply.github.com>
2023-07-24 15:06:26 +0330

    Update index.d.ts and improve startDrawingMode first param type

--- apps/image-editor/index.d.ts
+++ apps/image-editor/index.d.ts
@@ -321,7 +321,7 @@ declare namespace tuiImageEditor {
     public setObjectPosition(id: number, posInfo?: IPositionConfig): Promise<void>;
     public setObjectProperties(id: number, keyValue?: IGraphicObjectProps): Promise<void>;
     public setObjectPropertiesQuietly(id: number, keyValue?: IGraphicObjectProps): Promise<void>;
-    public startDrawingMode(mode: string, option?: { width?: number; color?: string }): boolean;
+    public startDrawingMode(mode: 'CROPPER' | 'FREE_DRAWING' | 'LINE_DRAWING' | 'TEXT' | 'SHAPE', option?: { width?: number; color?: string }): boolean;
     public stopDrawingMode(): void;
     public toDataURL(options?: IToDataURLOptions): string;
     public undo(iterationCount: number): Promise<any>;
